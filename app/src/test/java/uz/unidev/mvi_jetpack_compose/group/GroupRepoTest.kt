7e72459588de40c622a3e170a83d674749e94936
Nurlibay Koshkinbaev <koshkinbayevn@gmail.com>
2022-10-11 16:10:59 +0500

    unit test being added

---
+++ app/src/test/java/uz/unidev/mvi_jetpack_compose/group/GroupRepoTest.kt
@@ -0,0 +1,8 @@
+package uz.unidev.mvi_jetpack_compose.group
+
+/**
+ *  Created by Nurlibay Koshkinbaev on 11/10/2022 16:10
+ */
+
+class GroupRepoTest {
+}
---
+++ app/src/test/java/uz/unidev/mvi_jetpack_compose/room/GroupDaoTest.kt
@@ -0,0 +1,8 @@
+package uz.unidev.mvi_jetpack_compose.room
+
+/**
+ *  Created by Nurlibay Koshkinbaev on 11/10/2022 16:10
+ */
+
+class GroupDaoTest {
+}
